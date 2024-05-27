d7586ebd8887593a34768bccb98666feff2ab124
VitorDCabral <168486899+VitorDCabral@users.noreply.github.com>
2024-05-27 17:45:36 -0300

    Add files via upload

---
+++ aula 04/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const notaInput = document.getElementById('nota-input');
+    const addBtn = document.getElementById('add-button');
+    const calculateBtn = document.getElementById('calculate-button');
+    const notasDiv = document.getElementById('notas');
+    const mediaDiv = document.getElementById('media');
+
+    let notas = [];
+
+    const adicionarNota = () => {
+        const nota = parseFloat (notaInput.value);
+        
+        if (isNotaValida(nota)) {
+            notas.push(nota);
+            notaInput.value = '';
+            exibirNotas();
+        }
+       else {
+        alert ('Por favor, insira um numero de 0 a 10!');
+       }
+
+   };
+
+const isNotaValida = (nota) => {
+  return !isNaN(nota) && nota >= 0 && nota <= 10;
+}
+
+const exibirNotas = () => {
+    notasDiv.innerHTML = `<p>Notas: ${notas.join(', ')}</p>`;
+}
+
+const calculateBtn = () => {
+    
+}
+
+
+addBtn.addEventListener('click', adicionarNota);
+
+
+
+} );
+
+
+
+
